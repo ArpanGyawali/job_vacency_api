6a424e0815e808ec6c7f3bfe9bf4dcac019f9bd0
ArpanGyawali <arpan.gyawali7@gmail.com>
2021-08-07 14:28:06 +0545

    landing page costumized

--- client/src/Components/Layouts/Landing.js
+++ client/src/Components/Layouts/Landing.js
@@ -1,65 +1,128 @@
+// import React from 'react';
+// import { Link, Redirect } from 'react-router-dom';
+// import { connect } from 'react-redux';
+// import PropTypes from 'prop-types';
+
+// const Landing = ({ isAuthenticated }) => {
+// 	if (isAuthenticated) {
+// 		return <Redirect to='#' />;
+// 	}
+// 	return (
+// 		<section className='landing'>
+// 			<div className='dark-overlay'>
+// 				<div className='landing-inner'>
+// 					<h1 className='x-large'>Developer Connector</h1>
+// 					<h1 className='large'>A Jov Vacancy Web App</h1>
+// 					<p className='lead'>
+// 						Provides a job by various recruiter companies for the job seekers
+// 						out there
+// 					</p>
+// 					<div className='buttons'>
+// 						<Link
+// 							to={{
+// 								pathname: '/register',
+// 								role: 'seeker',
+// 							}}
+// 							className='btn btn-primary'
+// 						>
+// 							Register As Seeker
+// 						</Link>
+// 						<Link
+// 							to={{
+// 								pathname: '/register',
+// 								role: 'recruiter',
+// 							}}
+// 							className='btn btn-primary'
+// 						>
+// 							Register As Recruiter
+// 						</Link>
+// 						<br />
+// 						<br />
+// 						<Link
+// 							to={{
+// 								pathname: '/login',
+// 								role: 'seeker',
+// 							}}
+// 							className='btn btn-light'
+// 						>
+// 							Login As Seeker
+// 						</Link>
+// 						<Link
+// 							to={{
+// 								pathname: '/login',
+// 								role: 'recruiter',
+// 							}}
+// 							className='btn btn-light'
+// 						>
+// 							Login As Recruiter
+// 						</Link>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</section>
+// 	);
+// };
+
+// Landing.propTypes = {
+// 	isAuthenticated: PropTypes.bool,
+// };
+
+// const mapStateToProps = (state) => ({
+// 	isAuthenticated: state.auth.isAuthenticated,
+// });
+
+// export default connect(mapStateToProps)(Landing);
+
 import React from 'react';
 import { Link, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Fragment } from 'react';
+import main from '../../img/main.png';
+import WhatWeDo from './WhatWeDo';
+import GettingStarted from './GettingStarted';
+import Footer from './Footer';
+import './Landing.css';
 
 const Landing = ({ isAuthenticated }) => {
 	if (isAuthenticated) {
 		return <Redirect to='#' />;
 	}
 	return (
-		<section className='landing'>
-			<div className='dark-overlay'>
-				<div className='landing-inner'>
-					<h1 className='x-large'>Developer Connector</h1>
-					<h1 className='large'>A Jov Vacancy Web App</h1>
-					<p className='lead'>
-						Provides a job by various recruiter companies for the job seekers
-						out there
-					</p>
-					<div className='buttons'>
-						<Link
-							to={{
-								pathname: '/register',
-								role: 'seeker',
-							}}
-							className='btn btn-primary'
-						>
-							Register As Seeker
-						</Link>
-						<Link
-							to={{
-								pathname: '/register',
-								role: 'recruiter',
-							}}
-							className='btn btn-primary'
-						>
-							Register As Recruiter
-						</Link>
-						<br />
-						<br />
-						<Link
-							to={{
-								pathname: '/login',
-								role: 'seeker',
-							}}
-							className='btn btn-light'
-						>
-							Login As Seeker
-						</Link>
-						<Link
-							to={{
-								pathname: '/login',
-								role: 'recruiter',
-							}}
-							className='btn btn-light'
-						>
-							Login As Recruiter
-						</Link>
+		<Fragment>
+			<section class='banner_main'>
+				<div class='container'>
+					<div class='row d_flex'>
+						<div class='col-md-5'>
+							<div class='text-bg'>
+								<h1>The Unnamed</h1>
+								<span>A Job Hosting Web App</span>
+								<p>
+									Provides jobs by various recruiter companies for the people
+									seeking for jobs they want out there in convinient way{' '}
+								</p>
+								<a href='#service'>Our Service</a>
+								<a href='#get'>Get Started</a>
+							</div>
+						</div>
+						<div class='col-md-7'>
+							<div class='text-img'>
+								<figure>
+									<img src={main} alt='' />
+								</figure>
+							</div>
+						</div>
 					</div>
 				</div>
+			</section>
+			<div id='service'>
+				<WhatWeDo />
 			</div>
-		</section>
+			<div id='get'>
+				<GettingStarted />
+			</div>
+			<Footer />
+		</Fragment>
 	);
 };
 
---
+++ client/src/Components/Layouts/WhatWeDo.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import './Landing.css';
+import post from '../../img/post.png';
+import search from '../../img/search.png';
+import applients from '../../img/applients.png';
+import apply from '../../img/apply.png';
+import profile from '../../img/profile.png';
+import interview from '../../img/interview.png';
+
+const WhatWeDo = () => {
+	return (
+		<div id='service' class='Services'>
+			<div class='container'>
+				<div class='row'>
+					<div class='col-md-12'>
+						<div class='titlepage'>
+							<h1>OUR SERVICES</h1>
+							<p>
+								We do our best in satisfying you and fulfiling your needs <br />
+								The services we provide and things you can do through our app...
+							</p>
+						</div>
+					</div>
+				</div>
+				<div class='row'>
+					<div class='col-xl-4 col-lg-4 col-md-4 col-sm-12'>
+						<div class='Services-box'>
+							<i>
+								<img src={post} alt='#' />
+							</i>
+							<h3>Post jobs</h3>
+							<p>Companies can post the job specifying every details.</p>
+							<br />
+						</div>
+					</div>
+					<div class='col-xl-4 col-lg-4 col-md-4 col-sm-12'>
+						<div class='Services-box'>
+							<i>
+								<img src={search} alt='#' />
+							</i>
+							<h3>Search for jobs</h3>
+							<p>
+								Job seekers can easily find the jobs that they are looking for
+								provided by various companies.
+							</p>
+						</div>
+					</div>
+					<div class='col-xl-4 col-lg-4 col-md-4 col-sm-12'>
+						<div class='Services-box'>
+							<i>
+								<img src={apply} alt='#' />
+							</i>
+							<h3>Apply for jobs</h3>
+							<p>
+								If you are interested for some job, you can provide them their
+								resume and apply for it.
+							</p>
+						</div>
+					</div>
+					<div class='col-xl-4 col-lg-4 col-md-4 col-sm-12'>
+						<div class='Services-box'>
+							<i>
+								<img src={profile} alt='#' />
+							</i>
+							<h3>Add a profile</h3>
+							<p>
+								You can add and change your profile data so that other can
+								recognize you and see your provided information.
+							</p>
+						</div>
+					</div>
+					<div class='col-xl-4 col-lg-4 col-md-4 col-sm-12'>
+						<div class='Services-box'>
+							<i>
+								<img src={applients} alt='#' />
+							</i>
+							<h3>Know your applients</h3>
+							<p>Recruiters can see who applied for the job and know them.</p>
+							<br />
+						</div>
+					</div>
+					<div class='col-xl-4 col-lg-4 col-md-4 col-sm-12'>
+						<div class='Services-box'>
+							<i>
+								<img src={interview} alt='#' />
+							</i>
+							<h3>Call for interview</h3>
+							<p>
+								If you are selected among the applients, you will be called for
+								an interview. That happens outside our app.
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default WhatWeDo;
